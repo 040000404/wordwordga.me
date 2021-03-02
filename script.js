46e88d62baad9968d364eb9a51d9c86a455ab52b
Lee Haryung <50364669+040000404@users.noreply.github.com>
2021-03-02 21:34:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+const NAME = "once";
+var win1;
+var win2;
+let win3;
+let win4;
+let win5;
+let win6;
+let win7;
+let win8;
+let win9;
+// var event = new Event('lv2start');
+// var el = document.getElementById('6.html lv2');
+// el.addEventListener("click",lv2());
+jQuery(function($){
+$('#title1').html('small');
+$('#title2').html('entity');
+$('#title3').html('mutated');
+$('#title4').html('by');
+$('#title5').html('irresistible');
+$('#title6').html('situation');
+$('#title7').html('what');
+$('#title8').html('changes');
+$('#title9').html('earthworms');
+$('#title10').html('go');
+$('#title11').html('through');
+$('#title12').html('in');
+$('#title13').html('earth');
+$('#title14').html('quake');
+$('#title15').html('?');
+$('#title16').html('like');
+$('#title17').html('hurricane');
+$('#title18').html('warning');
+$('#title19').html('momentary');
+$('#title20').html('excitement');
+$('#title30').html('when');
+$('#title40').html('abstraction');
+$('#title50').html('hits');
+$('#title60').html('reality');
+$('#title70').html('what');
+$('#title80').html('is');
+$('#title90').html('the');
+$('#title100').html('percentage');
+$('#title110').html('of');
+$('#title120').html('salvation');
+$('#title130').html('after');
+$('#title140').html('that');
+$('#title150').html('?');
+
+$('#lv2').click(function() {
+
+
+     if(this.name !== NAME){
+       this.name = NAME;
+       win7 = window.open('7.html','_blank');
+       win8 = window.open('8.html');
+       win9 = window.open('9.html');
+       win10 = window.open('10.html');
+       win11 = window.open('11.html','_blank');
+       win12 = window.open('12.html');
+       win13= window.open('13.html');
+       win14 = window.open('14.html');
+       win15 = window.open('15.html');
+
+     }
+   });
+
+   $('#lv3').click(function() {
+
+        if(this.name !== NAME){
+          this.name = NAME;
+          win16 = window.open('16.html','_blank');
+          win17 = window.open('17.html');
+          win18 = window.open('18.html');
+          win19 = window.open('19.html');
+          win20 = window.open('20.html','_blank');
+          win30 = window.open('30.html');
+          win40= window.open('40.html');
+          win50 = window.open('50.html');
+          win60 = window.open('60.html');
+          win70= window.open('70.html','_blank');
+          win80= window.open('80.html');
+          win90 = window.open('90.html');
+          win100 = window.open('100.html');
+          win110 = window.open('110.html','_blank');
+          win120 = window.open('120.html');
+          win130= window.open('130.html');
+          win140 = window.open('140.html');
+          win150 = window.open('150.html');
+        }
+      });
+
+ });
+function close(){
+  self.close();
+}
+function lv1(){
+  if(this.name !== NAME){
+    this.name = NAME;
+    win2 = window.open('2.html');
+    win3 = window.open('3.html');
+    win4 = window.open('4.html');
+    win5 = window.open('5.html');
+    win6 = window.open('6.html');
+
+  }
+}
+//7.html
+// (function( $ ){
+//    $.fn.lv2 = function() {
+//      if(this.name !== NAME){
+//        this.name = NAME;
+//        win7 = window.open('7.html','_blank');
+//        win8 = window.open('8.html');
+//        win9 = window.open('9.html');
+//        win10 = window.open('10.html');
+//      }
+//
+//      $('#title1').html('what');
+//      $( "#body1" ).load( "7.html" );
+//       return this;
+//    };
+// })( jQuery );
+
+// $('#lv2').click(function() {
+//      if(this.name !== NAME){
+//        this.name = NAME;
+//        win7 = window.open('7.html','_blank');
+//        win8 = window.open('8.html');
+//        win9 = window.open('9.html');
+//        win10 = window.open('10.html');
+//      }
+//    });
+//
+// $('#lv2').click(function(){
+//   $( "#body1" ).load( "7.html #body7" );
+// });
+// function lv2(){
+//
+//
+//   win2.document.getElementById('title2').write('changes');
+//   win3.document.getElementById('title3').write('earthworms');
+//   win4.document.getElementById('title4').write('go');
+//   win5.document.getElementById('title5').write('through');
+//   win6.document.getElementById('title6').write('in');
+//
+// }
